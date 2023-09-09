0e14d8d57032b8ec8e4033482c5706852d75df11
surbhitiwari2023@gmail.com <surbhitiwari2023@gmail.com>
2023-09-10 02:34:55 +0530

    Initial commit

---
+++ intro-to-javascript/lesson 10/10f/10f.js
@@ -0,0 +1,8 @@
+function toggleButton(selector) {
+  const buttonElement = document.querySelector(selector);
+  if (buttonElement.classList.contains("is-toggled")) {
+    buttonElement.classList.remove("is-toggled");
+  } else {
+    buttonElement.classList.add("is-toggled");
+  }
+}
---
+++ intro-to-javascript/lesson 10/10j/10j.js
@@ -0,0 +1,15 @@
+let calculation = localStorage.getItem("calculation") || "";
+displayCalculation();
+function updateCalculation(value) {
+  calculation += value;
+  displayCalculation();
+  saveCalculation();
+}
+
+function saveCalculation() {
+  localStorage.setItem("calculation", calculation);
+}
+
+function displayCalculation() {
+  document.querySelector(".js-display-calc").innerHTML = calculation;
+}
